4455dddbd3de04d977760aa96b623caf4deeac4b
Galal Husseny <67426319+galal-husseny@users.noreply.github.com>
2021-07-04 15:53:28 +0200

    day1 php

---
+++ 1st/operators.php
@@ -0,0 +1,112 @@
+<?php 
+
+// $x = 5;
+// $x = 6;
+
+// define('per_page',30,true);
+
+
+// echo PER_PAGE;
+
+
+// OPERATORS
+
+// Arithmatic operators
+
+$firstNumber = 2;
+
+$secondNumber = 6;
+
+$power = 3;
+
+$result = $firstNumber ** $power;
+
+$maxSize = 10 ** 6;
+
+
+// echo $result;
+
+// assignment operators
+$x = 5;
+
+$x = $x / 1;
+$x /= 1;
+// echo $x;
+
+
+$select =   "<select >".
+                "<option> male </option>".
+                "<option> female </option>".
+            "</select>";
+// echo $select;
+$select =   "<select ><option> male</option><option> female </option></select>";
+
+
+
+// logical operators
+$number1 = 10;
+$number2 = 20;
+$number3 = 30;
+
+//  AND && ,
+// var_dump( !(($number1 > $number2 &&  ($number3 > $number2   OR 1 > 0)) And 5 > 6));
+
+//  or || ,
+//   not !
+
+
+
+// comparison operators
+
+// $number1 = 10;
+// $number2 = 20;
+// $number3 = 30;
+
+
+
+// var_dump($number3 > $number2 );
+
+// var_dump($number1 === $number2);
+
+// $x = '5';
+// $x += 1;
+
+// var_dump($x === '5');
+
+// increasement || decreasement
+// $number1 = 10;
+// $prefix = ++$number1;
+
+
+// $postfix = $number1++;
+
+// echo $postfix .'<br>';
+// echo $number1;
+
+// $x = 10;
+// echo $x++;
+// echo '<br>';
+// echo ++$x;
+
+// string operator
+$select = "<select>";
+$select .= "<option>male</option>";
+$select .= "<option>female</option>";
+$select .= "</select>";
+// echo $select;
+
+
+// scape operator
+$errorMessage = ' Required User Dose N\'t Exits ';
+
+// echo $errorMessage;
+
+
+// object operator
+
+// $userObject->propertyName;
+
+
+
+
+?>
